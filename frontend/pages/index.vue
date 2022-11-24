fec82be45d7dfe608e5f1d7bc5fca719bf0255ce
daichinagano <d.nagano1002@gmail.com>
2022-11-25 07:02:57 +0900

    💄 add tailwind cdd

---
+++ frontend/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="tw-w-48">
+    <div
+      class="tw-text-white tw-text-center tw-bg-clip-border tw-p-6 tw-bg-cyan-700"
+    >
+      Hello, world
+    </div>
+  </div>
+</template>

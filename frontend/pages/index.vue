df3d4fa50e59cf550b03992e116fca890740e010
daichinagano <d.nagano1002@gmail.com>
2022-11-25 11:08:06 +0900

    🚧 create index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="tw-w-48">
-    <div
-      class="tw-text-white tw-text-center tw-bg-clip-border tw-p-6 tw-bg-cyan-700"
-    >
-      Hello, world
+  <div class="tw-container tw-mx-auto tw-p-7">
+    <h1 class="tw-text-4xl tw-font-bold">Figmaの使い方について</h1>
+    <div class="tw-flex tw-text-2xl tw-mt-4">
+      <h1 class="tw-mr-10">こじゃ</h1>
+      <h1>2022/08/10</h1>
+    </div>
+    <div class="tw-flex tw-text-2xl tw-mt-6">
+      <h1 class="tw-mr-10">デザイン</h1>
+      <h1>フロント</h1>
+    </div>
+    <div class="tw-mt-6">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
     </div>
   </div>
 </template>

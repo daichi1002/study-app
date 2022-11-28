e852157c80de759b885688c9e6b3622c35b65e6e
daichinagano <d.nagano1002@gmail.com>
2022-11-28 20:09:15 +0900

    🔥 remove dir and code

--- backend/server/server.go
+++ backend/server/server.go
@@ -7,24 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// TODO
-// func NewGRPCServer(repos repository.Repositories) *grpc.Server {
-// 	options := make([]grpc.ServerOption, 0)
-// 	options = append(options, grpc.UnaryInterceptor(
-// 		middleware.ChainUnaryServer(
-// 			interceptor.ValidationInterceptor(),
-// 		)),
-// 	)
-
-// 	server := grpc.NewServer(options...)
-
-// 	pb.RegisterArticleServiceServer(server, service.NewArticleService(
-// 		usecase.NewArticleUsecase(repos.ArticleRepository),
-// 	))
-
-// 	return server
-// }
-
 func NewApiServer(router *gin.Engine, repos repository.Repositories) {
 	articleUsecase := usecase.NewArticleUsecase(repos.ArticleRepository)
 

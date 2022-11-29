3a73003bd29f2b83f8e099c6dabef1417beacaf7
daichinagano <d.nagano1002@gmail.com>
2022-11-29 19:24:22 +0900

    🎨 add root link

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bg-teal-300">
     <header class="container mx-auto justify-between flex items-center">
-      <img class="w-16" src="../assets/images/study.png" />
+      <NuxtLink to="/"
+        ><img class="w-16" src="../assets/images/study.png"
+      /></NuxtLink>
       <nav>
         <ul class="flex align-center">
           <li class="text-lg px-6"><NuxtLink to="/list">記録</NuxtLink></li>

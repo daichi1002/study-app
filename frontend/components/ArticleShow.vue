5f10e837b0ec89d8adb87bd03c6569d9f3a12b53
daichinagano <d.nagano1002@gmail.com>
2022-12-01 11:45:41 +0900

    💄 add edit button

--- frontend/components/ArticleShow.vue
+++ frontend/components/ArticleShow.vue
@@ -9,7 +9,6 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <h1 class="text-4xl font-bold">{{ props.value.title }}</h1>
   <div class="flex text-2xl mt-4">
     <h1 class="mr-10">{{ props.value.userName }}</h1>
     <h1>{{ props.value.updatedDate }}</h1>
--- frontend/pages/article/[id].vue
+++ frontend/pages/article/[id].vue
@@ -8,6 +8,12 @@ const { data } = await useFetch<Article>(`http://localhost:8080/article/${id}`);
 
 <template>
   <div class="container mx-auto p-7" v-if="data">
+    <div class="flex">
+      <h1 class="text-4xl font-bold mr-4">{{ data.title }}</h1>
+      <button class="w-16 h-10 py-2 px-4 rounded-lg">
+        <img src="../../assets/images/edit.png" />
+      </button>
+    </div>
     <ArticleShow :value="data" />
   </div>
 </template>

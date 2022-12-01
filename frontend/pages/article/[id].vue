0c42b3a3eff1bc561c2c202e97862e7257d0d98b
daichinagano <d.nagano1002@gmail.com>
2022-12-01 11:14:35 +0900

    ✨ add markdown editer preview

--- frontend/pages/article/[id].vue
+++ frontend/pages/article/[id].vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { Article } from "~/types/article";
 const router = useRoute();
-const id = router.params;
+const { id } = router.params;
 
-const { data } = await useFetch<Article>(`/article/${id}`);
-console.log(data);
+const { data } = await useFetch<Article>(`http://localhost:8080/article/${id}`);
 </script>
 
 <template>

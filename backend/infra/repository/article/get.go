d03862b21c524c75db3613262c08ceba0332aa80
daichinagano <d.nagano1002@gmail.com>
2022-12-01 12:06:37 +0900

    ⚡️ update get article sql

--- backend/infra/repository/article/get.go
+++ backend/infra/repository/article/get.go
@@ -3,6 +3,7 @@ package article
 import "backend/domain/model"
 
 func (r *articleRepository) ListArticles() (articles []*model.ResponseArticle, err error) {
+	// TODO タグIDをGROUP CONCATで取ると、タグをクリックして、該当タグごとに記事を表示する際にうまく検索できないと思うので、stringのスライスで受け取るようにしたい
 	err = r.DB.Table("articles as a").
 		Select(`
 			a.article_id,
@@ -10,11 +11,12 @@ func (r *articleRepository) ListArticles() (articles []*model.ResponseArticle, e
 			a.content,
 			a.updated_at,
 			u.name as user_name,
-			t.name as tag
+			GROUP_CONCAT(t.name separator ' / ') as tag
 		`).
 		Joins("left outer join users as u on u.user_id = a.user_id").
 		Joins("left outer join article_tags as at on at.article_id = a.article_id").
 		Joins("left outer join tags as t on t.id = at.id").
+		Group("a.article_id").
 		Find(&articles).Error
 
 	if err != nil {
@@ -32,12 +34,13 @@ func (r *articleRepository) ShowArticle(id string) (articles *model.ResponseArti
 			a.content,
 			a.updated_at,
 			u.name as user_name,
-			t.name as tag
+			GROUP_CONCAT(t.name separator ' / ') as tag
 		`).
 		Joins("left outer join users as u on u.user_id = a.user_id").
 		Joins("left outer join article_tags as at on at.article_id = a.article_id").
 		Joins("left outer join tags as t on t.id = at.id").
 		Where("a.article_id = ?", id).
+		Group("a.article_id").
 		Find(&articles).Error
 
 	if err != nil {

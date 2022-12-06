9e12b889d6b5907e7156a5727a67a431c6c931fd
daichinagano <d.nagano1002@gmail.com>
2022-12-06 10:14:01 +0900

    ✨ add edit view

--- frontend/pages/article/[id].vue
+++ frontend/pages/article/[id].vue
@@ -10,7 +10,7 @@ const { data } = await useFetch<Article>(`http://localhost:8080/article/${id}`);
   <div class="container mx-auto p-7" v-if="data">
     <div class="flex">
       <h1 class="text-4xl font-bold mr-4">{{ data.title }}</h1>
-      <NuxtLink to="">
+      <NuxtLink :to="`/article/edit/${id}`">
         <button class="w-16 h-10 py-2 px-4 rounded-lg">
           <img src="../../assets/images/edit.png" />
         </button>
--- frontend/pages/article/edit/[id].vue
+++ frontend/pages/article/edit/[id].vue
@@ -2,6 +2,13 @@
 import { Tag } from "~/types/tag";
 import { Article } from "~/types/article";
 
+const router = useRoute();
+const { id } = router.params;
+
+const { data: article } = await useFetch<Article>(
+  `http://localhost:8080/article/${id}`
+);
+
 const { data: tags, error } = await useFetch<Tag[]>(
   `http://localhost:8080/article/create`
 );
@@ -12,20 +19,13 @@ if (!tags.value || error.value) {
     message: "Tags not found",
   });
 }
-const reqArticle = reactive<Article>({
-  articleId: null,
-  title: "",
-  content: "",
-  tags: [],
-  updatedAt: null,
-});
 
 const saveArticle = async () => {
   const { data, error } = await useFetch<Article>(
-    "http://localhost:8080/article/create",
+    `http://localhost:8080/article/edit/${id}`,
     {
-      method: "POST",
-      body: reqArticle,
+      method: "PUT",
+      body: tags,
     }
   );
 
@@ -39,14 +39,17 @@ const saveArticle = async () => {
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <Form :value="reqArticle" :tags="tags" />
+  <div class="container mx-auto" v-if="article">
+    <Form :value="article" :tags="tags" />
     <div class="mt-4 grid grid-cols-9 gap-9">
       <button
         class="bg-teal-300 hover:bg-teal-500 py-2 px-4 rounded-lg"
         @click="saveArticle()"
       >
-        保存
+        編集
+      </button>
+      <button class="bg-red-400 hover:bg-red-500 py-2 px-4 rounded-lg">
+        削除
       </button>
     </div>
   </div>

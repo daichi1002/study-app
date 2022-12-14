57e1e2aaf15084e2a1e9359f9e3ada6c82347999
daichinagano <d.nagano1002@gmail.com>
2022-12-15 08:44:37 +0900

    💄 update ui

--- frontend/components/ArticleShow.vue
+++ frontend/components/ArticleShow.vue
@@ -12,12 +12,16 @@ const { data: users } = await useFetch<User[]>("http://localhost:8080/users");
 </script>
 
 <template>
-  <div class="flex text-2xl mt-4">
+  <div class="flex text-lg mt-2">
     <h1 class="mr-10">{{ setUserName(props.value.userId, users) }}</h1>
     <h1>{{ formatDate(props.value.updatedAt) }}</h1>
   </div>
-  <div class="flex text-2xl mt-6">
-    <h1 class="mr-10">{{ setTagName(props.value.tags) }}</h1>
+  <div class="flex text-lg mt-3 grid grid-cols-12">
+    <div v-for="tag in props.value.tags">
+      <div class="mr-2 border rounded-lg border-emerald-300 text-center">
+        {{ tag.tagName }}
+      </div>
+    </div>
   </div>
   <div class="mt-6">
     <md-editor v-model="props.value.content" preview-only />
